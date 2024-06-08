e28b5a27ca1c5655d3a4330ac9b59050e6bac068
nKadykov <nkadykov@gmail.com>
2024-06-08 19:15:18 +0300

    fix: coroutine.cpp

--- src/coroutine.cpp
+++ src/coroutine.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
+#include <iostream>
 
-void handle_client(boost::asio::ip::tcp::socket socket, boost::asio::yield_context yield) {
-    using namespace boost::asio;
+// асинхронное чтение из сокета 
+void async_read(boost::asio::ip::tcp::socket& socket, boost::asio::yield_context yield) {
     try {
-        char data[512];
-        for(;;) {
-            std::size_t length = socket.async_read_some(buffer(data), yield);
-            if(length > 0) {
-                async_write(socket, buffer(data, length), yield);
-            }
-        }
-    } catch(std::exception& ex) {
-        std::cerr << "Exception in handle_client: " << ex.what() << "\n";
-    } 
-}
-
-void accept_connection(boost::asio::io_context& io_context, unsigned short port, boost::asio::yield_context yield) {
-    using namespace boost::asio;
-    ip::tcp::acceptor acceptor(io_context, ip::tcp::endpoint(ip::tcp::v4(), port));
-    for(;;) {
-        ip::tcp::socket socket(io_context);
-        acceptor.async_accept(socket, yield);
-        spawn(io_context, std::bind(handle_client, std::move(socket), std::placeholders::_1));
+        char data[128];
+        // асинхронное чтение данных в буфер
+        std::size_t n = socket.async_read_some(boost::asio::buffer(data), yield);
+        std::cout.write(data, n);
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 }
 
 int main() {
-    using namespace boost::asio;
     try {
         // объект управления асинхронными операциями
-        io_context io_context;
-        // запуск корутины, которая принимает соединения и обрабатывает их
-        spawn(io_context, std::bind(accept_connection, std::ref(io_context)));
-        // запуск цикл событий и выполняет все асинхронные операции
+        boost::asio::io_context io_context;
+        // объект для поиска конечных точек по имени хоста и порту
+        boost::asio::ip::tcp::resolver resolver(io_context);
+        boost::asio::ip::tcp::resolver::results_type endpoints =
+            resolver.resolve("example.com", "daytime");
+        // создание tcp сокета
+        boost::asio::ip::tcp::socket socket(io_context);
+        // асинхронное подключение к конечным точкам
+        boost::asio::async_connect(socket, endpoints,
+            [&](const boost::system::error_code& ec, const boost::asio::ip::tcp::endpoint&) {
+                if (!ec) {
+                    boost::asio::spawn(io_context,
+                        [&](boost::asio::yield_context yield) {
+                            async_read(socket, yield);
+                        });
+                }
+            });
+        // цикл обработки событий, который запускает асинхронные операции и корутины
         io_context.run();
-    } catch(std::exception& ex) {
-        std::cerr << "Exception: " << ex.what() << "\n";
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
     return 0;
